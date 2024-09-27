f776dc04e67b8278a346b518b1d0718f0ec21141
umapiras <r.umamoto0815@gmail.com>
2024-09-27 22:42:18 +0900

    1st commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,5 @@ use App\Http\Controllers\PostController;
 |
 */
  
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}',[PostController::class , 'show']);
