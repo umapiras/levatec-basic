b8bac50a695310b29cb993e10e6df4b1d0d73e7e
umapiras <r.umamoto0815@gmail.com>
2024-10-01 23:33:32 +0900

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 use App\Models\Post;
 
+
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -11,9 +13,21 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts'=>$post->getPaginateByLimit(3)]);
     }
+
     public function show(Post $post)
     {
     return view('posts.show')->with(['post' => $post]);
     }    
-    
+
+   public function create()
+   {
+    return view ('posts.create');
+   }
+
+   public function store(PostRequest $request, Post $post)
+   {
+       $input = $request['post'];
+       $post->fill($input)->save();
+       return redirect('/posts/' . $post->id);
+   }
 }
